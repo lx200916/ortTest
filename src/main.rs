1d7f412ff5efe150617ee027632412134cb6449c
Lee <44310445+lx200916@users.noreply.github.com>
2023-08-03 15:48:00 +0800

    Add Time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, default};
 use std::rc::Rc;
 use ort::{Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder, Value};
 use ort::execution_providers::NNAPIExecutionProviderOptions;
@@ -6,11 +6,17 @@ use ort::session::Input;
 use ndarray::prelude::*;
 use ndarray::Array;
 use ort::tensor::OrtOwnedTensor;
+use std::env;
 
 fn main()-> OrtResult<()> {
     tracing_subscriber::fmt::init();
     let mut stdout = io::stdout();
     // let mut rng = rand::thread_rng();
+    let args: Vec<String> = env::args().collect::<Vec<_>>();
+    let model_name = &args[1];
+    let default_number = String::from("10");
+    let number = args.get(2).unwrap_or(&default_number);
+    let number = number.parse::<usize>().unwrap();
 
     let environment = Environment::builder()
         .with_name("model_test")
@@ -25,7 +31,7 @@ fn main()-> OrtResult<()> {
     let session = SessionBuilder::new(&environment)?
         .with_optimization_level(GraphOptimizationLevel::Level3)?
         .with_intra_threads(1)?
-        .with_model_from_file("model_vision_s.onnx")?;
+        .with_model_from_file(model_name)?;
     let mut inputs:Vec<CowArray<f32,IxDyn>> = Vec::new();
     for Input{name,input_type,dimensions}  in session.inputs.iter(){
         println!("name: {:?}, input_type: {:?}, dimensions: {:?}",name,input_type,dimensions);
@@ -35,15 +41,22 @@ fn main()-> OrtResult<()> {
         // let array = Value::from_array(session.allocator(), )?;
         inputs.push(array);
     }
-    let inputs = inputs.iter().map(|input| Value::from_array(session.allocator(),input).unwrap()).collect::<Vec<_>>();
-    let outputs: Vec<Value> = session.run(inputs)?;
-    let output = outputs[0].try_extract()? as OrtOwnedTensor<f32, _>;
+    let mut time_elapsed = Vec::new();
+    for _ in 0..number{
+        let inputs: Vec<Value<'_>> = inputs.iter().map(|input: &ArrayBase<ndarray::CowRepr<'_, f32>, Dim<ndarray::IxDynImpl>>| Value::from_array(session.allocator(),input).unwrap()).collect::<Vec<Value>>();
+        let time_start = std::time::Instant::now();
+        let outputs: Vec<Value> = session.run(inputs)?;
+        println!("!!! BEGIN!");
+        let time_end = std::time::Instant::now();
+        let time = time_end.duration_since(time_start).as_millis();
+        time_elapsed.push(time);
+        println!("!!! END!");
+
+        let output = outputs[0].try_extract()? as OrtOwnedTensor<f32, _>;
         println!("{:?}",output.view().shape());
-    // println!("{:?}", inputs);
-
-
-
-    // session.run()
+    }
+    let time_elapsed = time_elapsed.iter().sum::<u128>() as f64 / time_elapsed.len() as f64;
+    println!("time_elapsed: {:?} ms",time_elapsed);
     Ok(())
 
 }
