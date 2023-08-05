44f9a402f15420546c9d9eddd401ddd2fbe4b63b
Lee <44310445+lx200916@users.noreply.github.com>
2023-08-05 11:11:15 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use std::{io, default};
 use std::rc::Rc;
-use ort::{Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder, Value};
+use ort::sys::OrtAllocator;
+use ort::{Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder, Value, Session};
 use ort::execution_providers::NNAPIExecutionProviderOptions;
 use ort::session::Input;
 use ndarray::prelude::*;
 use ndarray::Array;
 use ort::tensor::OrtOwnedTensor;
 use std::env;
-
+enum InputTensor {
+    Int64(CowArray<'static,i64,IxDyn>),
+    Float32(CowArray<'static,f32,IxDyn>),
+}
+impl InputTensor {
+    fn to_tensor(& self,allocator:*mut OrtAllocator) -> Box<Value> {
+        match self {
+            InputTensor::Int64(array) => Box::new(Value::from_array(allocator,&array).unwrap()),
+            InputTensor::Float32(array) => Box::new(Value::from_array(allocator,&array).unwrap()),
+        }
+    }
+    
+}
 fn main()-> OrtResult<()> {
     tracing_subscriber::fmt::init();
-    let mut stdout = io::stdout();
     // let mut rng = rand::thread_rng();
     let args: Vec<String> = env::args().collect::<Vec<_>>();
     let model_name = &args[1];
@@ -32,18 +44,32 @@ fn main()-> OrtResult<()> {
         .with_optimization_level(GraphOptimizationLevel::Level3)?
         .with_intra_threads(1)?
         .with_model_from_file(model_name)?;
-    let mut inputs:Vec<CowArray<f32,IxDyn>> = Vec::new();
+    let mut inputs:Vec<InputTensor> = Vec::new();
+    // let mut i64_inputs:Vec<CowArray<i64,IxDyn>> = Vec::new();
     for Input{name,input_type,dimensions}  in session.inputs.iter(){
-        println!("name: {:?}, input_type: {:?}, dimensions: {:?}",name,input_type,dimensions);
         let dimensions:Vec<usize> = dimensions.iter().map(|d| return if d.is_some() { d.unwrap() as usize } else { 1 }).collect();
-        let array = CowArray::from(Array::<f32, _>::zeros(dimensions)).into_dyn();
-        // let array = Rc::new(array);
-        // let array = Value::from_array(session.allocator(), )?;
-        inputs.push(array);
+        println!("name: {:?}, input_type: {:?}, dimensions: {:?}",name,input_type,dimensions);
+        match input_type {
+            ort::tensor::TensorElementDataType::Float32 =>{
+                let array = CowArray::from(Array::<f32, _>::zeros(dimensions)).into_dyn();
+                // let array = Rc::new(array);
+                // let array = Value::from_array(session.allocator(), )?;
+                inputs.push(InputTensor::Float32(array));
+                
+            }
+            ort::tensor::TensorElementDataType::Int64 =>{
+                let array = CowArray::from(Array::<i64, _>::zeros(dimensions)).into_dyn();
+                // let array = Rc::new(array);
+                // let array = Value::from_array(session.allocator(), )?;
+                inputs.push(InputTensor::Int64(array));
+            }
+            _ => {}
+        }
+
     }
     let mut time_elapsed = Vec::new();
     for _ in 0..number{
-        let inputs: Vec<Value<'_>> = inputs.iter().map(|input: &ArrayBase<ndarray::CowRepr<'_, f32>, Dim<ndarray::IxDynImpl>>| Value::from_array(session.allocator(),input).unwrap()).collect::<Vec<Value>>();
+        let inputs: Vec<Value<'_>> = inputs.iter().map(|input| *input.to_tensor(session.allocator())).collect::<Vec<Value>>();
         let time_start = std::time::Instant::now();
         let outputs: Vec<Value> = session.run(inputs)?;
         println!("!!! BEGIN!");
